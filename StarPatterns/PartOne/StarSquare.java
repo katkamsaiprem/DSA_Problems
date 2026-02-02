5e61ce25f9312ba989bf9ab06c79fc4829b9d345
Saiprem <katukamsaiprem@gmail.com>
2026-02-02 10:22:54 +0530

    Add Java complete notes and implement StarSquare pattern generator
    - Added Java_Complete_Notes.pdf containing comprehensive Java syntax notes.
    - Created StarSquare.java to generate a square pattern of asterisks.

---
+++ StarPatterns/PartOne/StarSquare.java
@@ -0,0 +1,19 @@
+package StarPatterns.PartOne;
+// ****
+// ****
+// ****
+// ****
+public class StarSquare {
+
+    void main() {
+
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
